5a67d92c53078374227c7f189d3a64578e21b179
king7hills <haasjh1@gmail.com>
2025-02-04 14:48:26 -0500

    encapsulated dialog logic

--- src/dialog.js
+++ src/dialog.js
@@ -1,54 +1,58 @@
-/* import { storage, tFunc } from "./index.js";
+import { storage, tFunc } from "./index.js";
 
 // Diaglog main
-const dialog = document.querySelector('dialog');
-const closeDialog = document.querySelector('button.dialog_close');
-const newTaskButton = document.querySelector('button.newTask');
+function setupDialog() {
+    const dialog = document.querySelector('dialog');
+    const closeDialog = document.querySelector('button.dialog_close');
+    const newTaskButton = document.querySelector('button.newTask');
 
-newTaskButton.addEventListener("click", () => {
-    dialog.showModal();
-});
+    newTaskButton.addEventListener("click", () => {
+        dialog.showModal();
+    });
 
-closeDialog.addEventListener('click', () => {
-    dialog.close();
-});
+    closeDialog.addEventListener('click', () => {
+        dialog.close();
+    });
 
-const form = document.querySelector('form');
+    const form = document.querySelector('form');
 
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    formStuff = new FormData(form);
-    let newTask = formStuff.get('Task');
-    let newDueDate = formStuff.get('Due Date');
-    let newPriority = formStuff.get('Priority');
-    let newProject = formStuff.get('Project');
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        let formStuff = new FormData(form);
+        let newTask = formStuff.get('Task');
+        let newDueDate = formStuff.get('Due Date');
+        let newPriority = formStuff.get('Priority');
+        let newProject = formStuff.get('Project');
+            
+        const freshTask = tFunc.createTask(newTask, newDueDate, newPriority, newProject);
+
+        if (!storage.selectProject(newProject)) {
+            storage.createProject(newProject);
+            console.log("new project created");
+        };
+        tFunc.addTask(storage.selectProject(newProject), freshTask);
+        storage.saveData();
+        dialog.close();
+        form.reset();
+    });
+
+    function addProjectOption () {
+        //Generates new project option for form
+        const newProject = document.createElement("option");
+        const projectName = prompt("Name: ");
+        newProject.value = projectName;
+        newProject.name = projectName;
         
-    const freshTask = tFunc.createTask(newTask, newDueDate, newPriority, newProject);
+        //Changes selected project option to the newly created project
+        const defaultProjectSelection = document.querySelector("[value=general]");
+        defaultProjectSelection.removeAttribute("selected");
+        newProject.setAttribute("selected", "selected");
 
-    if (!storage.selectProject(newProject)) {
-        storage.createProject(newProject);
-        console.log("new project created");
+        document.querySelector("select").add(newProject);
     };
-    tFunc.addTask(storage.selectProject(newProject), freshTask);
-    storage.saveData();
-    dialog.close();
-    form.reset();
-});
 
-function addProjectOption () {
-    //Generates new project option for form
-    const newProject = document.createElement("option");
-    const projectName = prompt("Name: ");
-    newProject.value = projectName;
-    newProject.name = projectName;
-    
-    //Changes selected project option to the newly created project
-    const defaultProjectSelection = document.querySelector("[value=general]");
-    defaultProjectSelection.removeAttribute("selected");
-    newProject.setAttribute("selected", "selected");
+    const newProjectButton = document.querySelector("#form_new_project");
+    newProjectButton.onclick = addProjectOption;
+}
 
-    document.querySelector("select").add(newProject);
-};
-
-const newProjectButton = document.querySelector("#form_new_project");
-newProjectButton.onclick(addProjectOption()); */
+export { setupDialog };
--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import { DynamicPage } from "./dynamicPage.js";
 import { Task, tFunc } from "./task.js";
 import { storage } from "./storage.js";
 import { init_home } from "./home.js";
+import { setupDialog } from './dialog.js';
 
 export { DynamicPage } from "./dynamicPage.js";
 export { Task, tFunc } from "./task.js";
 export { storage } from "./storage.js";
 
 init_home();
+setupDialog();
 
 function loadContent (initFunction) {
     const page = document.querySelector('div#content');
